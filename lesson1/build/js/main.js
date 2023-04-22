5427134c7d361c7a4f61450c33192aa51170cd6b
Marco Rossel <andresmarcorossel@gmail.com>
2023-04-22 17:48:36 -0400

    typeScript | lesson 1 | type error

--- lesson1/build/js/main.js
+++ lesson1/build/js/main.js
@@ -1,3 +1,9 @@
 "use strict";
 var username = 'Marco';
 console.log(username);
+var a = 6;
+var b = 2;
+var c = 10;
+console.log(a * c);
+console.log(c / b);
+// viendo los errores de tipo 
--- lesson1/src/main.ts
+++ lesson1/src/main.ts
@@ -1,3 +1,13 @@
 let username = 'Marco';
 
 console.log(username);
+
+let a:number = 6;
+let b:number = 2;
+let c:string = '10';
+
+console.log(a * c);
+console.log(c / b);
+
+// viendo los errores de tipo 
+
