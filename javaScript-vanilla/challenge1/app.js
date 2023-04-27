600a091e6a0ce5fac6c6afad327a02ae101d2b61
Marco Rossel <andresmarcorossel@gmail.com>
2023-04-27 08:13:07 -0400

    vanilla project | challenge 1 | 2

---
+++ javaScript-vanilla/challenge1/app.js
@@ -0,0 +1,35 @@
+const assets = {
+    person1: {
+        name: "Lushito Random",
+        img: "/img/1",
+        para: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti, dolorum molestias modi atque doloremque ab repellat minima suscipit, voluptatem animi architecto necessitatibus amet esse quibusdam eligendi molestiae obcaecati praesentium saepe?"
+    },
+    person2: {
+        name: "Maarco Random",
+        img: "/img/2",
+        para: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti, dolorum molestias modi atque doloremque ab repellat minima suscipit, voluptatem animi architecto necessitatibus amet esse quibusdam eligendi molestiae obcaecati praesentium saepe?"
+    },
+    person3: {
+        name: "Eva Random",
+        img: "/img/3",
+        para: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti, dolorum molestias modi atque doloremque ab repellat minima suscipit, voluptatem animi architecto necessitatibus amet esse quibusdam eligendi molestiae obcaecati praesentium saepe?"
+    },
+    person4: {
+        name: "Eduardo Random",
+        img: "/img/4",
+        para: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti, dolorum molestias modi atque doloremque ab repellat minima suscipit, voluptatem animi architecto necessitatibus amet esse quibusdam eligendi molestiae obcaecati praesentium saepe?"
+    },
+    person5: {
+        name: "Juanito Random",
+        img: "/img/5",
+        para: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti, dolorum molestias modi atque doloremque ab repellat minima suscipit, voluptatem animi architecto necessitatibus amet esse quibusdam eligendi molestiae obcaecati praesentium saepe?"
+    },
+}
+
+console.log(assets.person1.para);
+
+(function () {
+    "use strict";
+
+    
+})()
