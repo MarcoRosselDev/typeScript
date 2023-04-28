cb962a65a2c5840d832c913910da7a4d6b9538ce
Marco Rossel <andresmarcorossel@gmail.com>
2023-04-27 22:21:19 -0400

    vanilla project | challenge 1 | 5 iterador assets

--- javaScript-vanilla/challenge1/app.js
+++ javaScript-vanilla/challenge1/app.js
@@ -1,27 +1,27 @@
 const assets = [
     {
         "name": "Lushito Random",
-        "img": "/img/1",
+        "img": "img/1.jpg",
         "para": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti, dolorum molestias modi atque doloremque ab repellat minima suscipit, voluptatem animi architecto necessitatibus amet esse quibusdam eligendi molestiae obcaecati praesentium saepe?"
     },
     {
         "name": "Maarco Random",
-        "img": "/img/2",
+        "img": "img/2.jpg",
         "para": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. repellat minima suscipit, voluptatem animi architecto necessitatibus amet esse quibusdam eligendi molestiae obcaecati praesentium saepe?"
     },
     {
         "name": "Eva Random",
-        "img": "/img/3",
+        "img": "img/3.jpg",
         "para": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur porro officiis omnis minima, libero nam autem doloribus deserunt! Enim, atque id! Quo quidem iste laborum suscipit perspiciatis adipisci saepe nesciunt. Blanditiis"
     },
     {
         "name": "Eduardo Random",
-        "img": "/img/4",
+        "img": "img/4.jpg",
         "para": "Quo quidem iste laborum suscipit perspiciatis adipisci saepe nesciunt. Blanditiis nisi laborum, corrupti voluptate quasi expedita nobis iusto qui, inventore aperiam quis explicabo tempora. Laboriosam delectus earum corporis cum necessitatibus aut. Sit provident laudantium"
     },
     {
         "name": "Juanito Random",
-        "img": "/img/5",
+        "img": "img/5.jpg",
         "para": 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur porro officiis omnis minima, libero nam autem doloribus deserunt! Enim, atque id! Quo quidem iste laborum suscipit perspiciatis adipisci saepe nesciunt. Blanditiis nisi laborum, corrupti voluptate quasi expedita nobis iusto qui, inventore aperiam quis explicabo tempora. Laboriosam delectus earum corporis cum necessitatibus aut. Sit provident laudantium quibusdam pariatur harum enim ducimus!'
     }
 ]
@@ -30,16 +30,29 @@ console.log(assets[1].img);
 
 let iterador = 0;
 
+// botones
 const btnSiguiente = document.querySelector('.nextBtn');
-const para = document.querySelector('.para');
+const btnAnterior = document.querySelector('.leftBtn');
 
-function map() {
+const para = document.querySelector('.para');
+const img = document.querySelector('.img');
+const name = document.querySelector('.name')
+
+function siguiente() {
     para.innerText = assets[iterador].para;
-    iterador++
+    img.src = assets[iterador].img;
+    name.innerText = assets[iterador].name;
 }
 
-map()
+siguiente()
 
 btnSiguiente.addEventListener('click', function () {
-    map()
+    iterador++;
+    siguiente();
+    console.log(iterador);
+})
+btnAnterior.addEventListener('click', function () {
+    iterador--;
+    siguiente()
+    console.log(iterador);
 })
