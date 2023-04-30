d603ef2bba88049803042e7963c3d2690c8e8a09
Marco Rossel <andresmarcorossel@gmail.com>
2023-04-30 13:16:59 -0400

    javaScript vanilla | project 2 | weather app | 8 fetch api | reminder to do list 5

--- javaScript-vanilla/project2-weatherApp/reminder/app.js
+++ javaScript-vanilla/project2-weatherApp/reminder/app.js
@@ -3,10 +3,25 @@
 
     const send = document.querySelector('.send');
     const inputValue = document.querySelector('input');
+    const divPrint = document.querySelector('.printDiv');
 
     send.addEventListener('click', function (event) {
         event.preventDefault();
-        console.log(inputValue.value);
+        divPrint.innerHTML = `
+        <li>
+        ${inputValue.value}</li>
+        `;
+        inputValue.value = ''
     })
-    console.log();
-})()
+})() 
+
+/* <main>
+        <h1>To do List</h1>
+        <form action="">
+            <input type="text" placeholder="write a task to do...">
+            <button class="send">Send</button>
+        </form>
+        <div class="printDiv">
+            
+        </div>
+    </main> */
