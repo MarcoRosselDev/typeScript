fb0bee3bcb7af7a6fa7ae7e86e58c463778d2aee
Marco Rossel <andresmarcorossel@gmail.com>
2023-04-30 00:00:18 -0400

    javaScript vanilla | project 2 | weather app | 8 fetch api | reminder to do list 2

--- javaScript-vanilla/project2-weatherApp/reminder/app.js
+++ javaScript-vanilla/project2-weatherApp/reminder/app.js
@@ -1,3 +1,9 @@
 (function () {
+    'use strict'
+
+    const send = document.querySelector('.send');
+    send.addEventListener('click', function () {
+        console.log('clicked');
+    })
     console.log('loaded');
 })()
