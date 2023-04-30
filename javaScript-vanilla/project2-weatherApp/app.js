8349346aba1038820a5f87183306a78a083c8b79
Marco Rossel <andresmarcorossel@gmail.com>
2023-04-29 23:43:44 -0400

    javaScript vanilla | project 2 | weather app | 8 fetch api

---
+++ javaScript-vanilla/project2-weatherApp/app.js
@@ -0,0 +1,17 @@
+
+
+async function weather(cityValue) {
+    try {
+        const a = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apiKey}&units=metric`);
+        console.log(a);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+weather('london');
+
+/* Response {type: 'cors', url: 'https://api.openweathermap.org/data/2.5/weather?q=…pid=31ac23275854f9c11d7b50ea72b823b8&units=metric', redirected: false, status: 200, ok: true, …}
+
+
+funciono*/
