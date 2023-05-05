9779a363ab7e8746ad86cc21bc9ba13dea9eeb5f
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-05 14:17:41 -0400

    javaScript vanilla | project 2 | weather app | 8 fetch api | reminder to do list 17

--- javaScript-vanilla/project2-weatherApp/reminder/app.js
+++ javaScript-vanilla/project2-weatherApp/reminder/app.js
@@ -4,29 +4,23 @@
     const send = document.querySelector('.send');
     const inputValue = document.querySelector('input');
     const divPrint = document.querySelector('.printDiv');
-    const localStrg = JSON.parse(localStorage.getItem('randomName'))
-
-    console.log();
 
     // variable let para almacenar la lista
     let saveList = []
 
-    
-
     send.addEventListener('click', function (event) {
         event.preventDefault();
         saveList.push(inputValue.value)
-        localStorage.setItem('randomName', JSON.stringify(saveList))
         function printDOM (){
             let finalPrint = '';
             for (let i = 0; i < saveList.length; i++) {
                 finalPrint += `
                 <div class="list">
-                    <p>${saveList[i]}</p>
-                    <div class="btns">
-                        <button class="btnDone">Finished</button>
-                        <button class="btnDelete">Delete</button>
-                    </div>                
+                <p>${saveList[i]}</p>
+                <div class="btns">
+                <button class="btnDone">Finished</button>
+                <button class="btnDelete">Delete</button>
+                </div>                
                 </div>
                 `
             }
@@ -39,16 +33,15 @@
             e.preventDefault()
 
             const thiss = this.parentNode.parentNode;
-
+            
             const f = thiss.querySelector('p');
             const text = f.innerText
-
+            
             saveList = saveList.filter(e => e !== text);
             console.log(saveList);
             this.parentNode.parentNode.remove()
         }))
     })
-
 })() 
 
 
