47c210bec73bd644415a25db0c7655c6d9cb5237
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-05 00:13:20 -0400

    javaScript vanilla | project 2 | weather app | 8 fetch api | reminder to do list 16

--- javaScript-vanilla/project2-weatherApp/reminder/app.js
+++ javaScript-vanilla/project2-weatherApp/reminder/app.js
@@ -4,13 +4,19 @@
     const send = document.querySelector('.send');
     const inputValue = document.querySelector('input');
     const divPrint = document.querySelector('.printDiv');
+    const localStrg = JSON.parse(localStorage.getItem('randomName'))
+
+    console.log();
 
     // variable let para almacenar la lista
     let saveList = []
 
+    
+
     send.addEventListener('click', function (event) {
         event.preventDefault();
         saveList.push(inputValue.value)
+        localStorage.setItem('randomName', JSON.stringify(saveList))
         function printDOM (){
             let finalPrint = '';
             for (let i = 0; i < saveList.length; i++) {
