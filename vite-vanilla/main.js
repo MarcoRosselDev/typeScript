f9fe578e80b2617e31f0bbe56aef40f54c72020b
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-08 16:41:56 -0400

    weather app | innerText api content

--- vite-vanilla/main.js
+++ vite-vanilla/main.js
@@ -2,42 +2,65 @@ import './style.css'
 
 const apiKey = import.meta.env.VITE_WHEATER_KEY
 
-async function weather(cityValue) {
-  try {
-      const a = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apiKey}&units=metric`);
-      console.log(a);
-  } catch (error) {
-      console.log(error);
-  }
-}
-
-weather('london');
-
 document.querySelector('#app').innerHTML = `
-  <div class="container">
-    <h2>Weather App</h2>
-    <form action="">
-        <input type="text" placeholder="input the city name...">
-        <!-- <button type="submit">Get Weather</button> -->
-        <!-- podemos add a button pero el input type submit lo envia por defecto
-        con el button tendriamos que escuchar el evento click -->
-        <input type="submit" value="Get Weather" class="btn">
-    </form>
-    <div class="details">
-        <img src="#" alt="icon weather" class="icon">
-        <h2 class="temperature">4 ° C</h2>
-        <p class="description">overcast clouds</p>
-        <div class="anotherDetails">
-            <div class="box">
-                <p>Feels like 3 ° C</p>
-            </div>
-            <div class="box">
-                <p>Humidity 81%</p>
-            </div>
-            <div class="box">
-                <p>Wind speed: 154 m/s</p>
+    <div class="container">
+        <h2>Weather App</h2>
+        <form action="">
+            <input type="text" placeholder="input the city name...">
+            <!-- <button type="submit">Get Weather</button> -->
+            <!-- podemos add a button pero el input type submit lo envia por defecto
+            con el button tendriamos que escuchar el evento click -->
+            <input type="submit" value="Get Weather" class="btn">
+        </form>
+        <div class="details">
+            <img src="#" alt="icon weather" class="icon">
+            <h2 class="temperature">4 ° C</h2>
+            <p class="description">overcast clouds</p>
+            <div class="anotherDetails">
+                <div class="box">
+                    <p>Feels like 3 ° C</p>
+                </div>
+                <div class="box">
+                    <p>Humidity 81%</p>
+                </div>
+                <div class="box">
+                    <p>Wind speed: 154 m/s</p>
+                </div>
             </div>
         </div>
     </div>
-  </div>
 `
+
+
+async function weather(cityValue) {
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apiKey}&units=metric`);
+        
+        if (!response.ok) {
+            throw new Error('La respuesta no fue bien')
+        }
+        
+        const data = await response.json()
+
+        const description = data.weather[0].description;
+        const icon = data.weather[0].icon;
+        const temperature = `${Math.round(data.main.temp)} ° C`;
+
+        const descripText = document.querySelector('.description').innerText = description;
+        const tempText = document.querySelector('.temperature').innerText = temperature;
+
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+const btn = document.querySelector('.btn');
+const valueInpt = document.querySelector('input')
+
+btn.addEventListener('click', function (e) {
+    e.preventDefault()
+    console.log('clicked');
+    weather(valueInpt.value);
+})
