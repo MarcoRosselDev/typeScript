9ea9561658441b94bc6f0ccb55da098fcbf3f07d
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-10 12:13:20 -0400

    weather app reminder | 2

--- vite-vanilla-reminder/main.js
+++ vite-vanilla-reminder/main.js
@@ -2,7 +2,8 @@ import './style.css';
 
 const key = import.meta.env.VITE_WHEATER_KEY_2;
 
-document.querySelector('#app').innerHTML = `
+/* document.querySelector('.app').innerHTML = `
   <div>
   </div>
 `;
+ */
