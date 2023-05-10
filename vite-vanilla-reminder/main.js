ed47b4b95e29fd26424d13319cfce524f3ba8974
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-10 14:42:41 -0400

    weather app reminder | 4

--- vite-vanilla-reminder/main.js
+++ vite-vanilla-reminder/main.js
@@ -2,6 +2,17 @@ import './style.css';
 
 const key = import.meta.env.VITE_WHEATER_KEY_2;
 
+// data 3.0
+//const url = `https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${key}`
+
+async function latLong(city) {
+  const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${key}`);
+  const data = await response.json()
+  console.log(data);
+}
+
+latLong(sydney);
+
 /* document.querySelector('.app').innerHTML = `
   <div>
   </div>
