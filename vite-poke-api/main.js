4bf5d0dbec072da762dc2356e224f8c7bb33e741
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-10 22:14:19 -0400

    vite poke api | 3

--- vite-poke-api/main.js
+++ vite-poke-api/main.js
@@ -1,23 +1,43 @@
 import './style.css';
 
 async function pokeApi(namePokemon) {
-  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${namePokemon}/`);
-  const data = await response.json();
 
-  if (!data) {
-    console.log('no se encontraron datos');
-  } else{
-    console.log(data);
+  try {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${namePokemon}/`);
+    const data = await response.json();
+    if (!data) {
+      console.log('no se encontraron datos');
+    } else{
+      console.log(data);
+
+      const imgFromnt = data.sprites.front_default;
+      console.log(imgFromnt);
+
+      result.innerHTML = `
+        <img src=${imgFromnt} alt="pokemon testing">
+      `
+    }
+  } catch (error) {
+    throw new Error('ocurrio un error inesperado !');
   }
 }
 
-pokeApi('charmander');
+const input = document.querySelector('.inpt');
+const btnSearch = document.querySelector('.btnSearch');
+const result = document.querySelector('.result');
+
+btnSearch.addEventListener('click', function (event) {
+  event.preventDefault();
+  pokeApi(input.value)
+})
+
+//pokeApi('charmander');
 
 /* response successful
 {abilities: Array(2), base_experience: 62, forms: Array(1), game_indices: Array(20), height: 6, …}
 */
 
-document.querySelector('#app').innerHTML = `
+/* document.querySelector('#app').innerHTML = `
   <div>
   </div>
-`;
+`; */
