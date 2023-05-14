28426fbeddcc82f5115334b4a74b4de96f2dce28
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-14 01:24:30 -0400

    photo app | 4 firt fetch successful

--- vite-photo-app/main.js
+++ vite-photo-app/main.js
@@ -1,10 +1,27 @@
 import './style.css';
 
 const publicKey = import.meta.env.VITE_KEY_UNSPLASH;
-console.log(publicKey);
+const div = document.querySelector('#app')
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <p>test photo api</p>
-  </div>
-`;
+async function getAPhoto() {
+
+  try {
+    const response = await fetch(`https://api.unsplash.com/photos/?client_id=${publicKey}`);
+    const data = await response.json();
+    console.log(data);
+
+    if (!data) {
+      // push dom a div with a message like "no se encontraron resultados";
+    } else {
+      let finalDom = '';
+      for (let i = 0; i < data.length; i++) {
+        finalDom += `<p>${data[i].alt_description}</p>`;
+      }
+      div.innerHTML = finalDom;
+    }
+  } catch (error) {
+    console.log('se produjo un error en la funcion getAPhoto');
+  }
+}
+
+getAPhoto();
