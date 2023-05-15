517fb46716fef80c4b8a907c80db19025a7316a3
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-15 14:06:11 -0400

    three-vite | 1

---
+++ three-vite/main.js
@@ -0,0 +1,12 @@
+import './style.css';
+import * as THREE from 'three';
+
+console.log();
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <p>
+      testing font family
+    </p>
+  </div>
+`;
