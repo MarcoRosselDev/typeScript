041154808f3695cd45192783a11b07d1549e0c16
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-15 17:59:37 -0400

    three-vite | 4

--- three-vite/main.js
+++ three-vite/main.js
@@ -2,7 +2,20 @@ import './style.css';
 import * as THREE from 'three';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
+
+const loader = new GLTFLoader();
+
+/* loader.load( 'path/to/model.glb', function ( gltf ) {
+
+	scene.add( gltf.scene );
+
+}, undefined, function ( error ) {
+
+	console.error( error );
+
+} ); */
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth/2, window.innerHeight/2, false);
@@ -21,9 +34,9 @@ camera.position.z = 5;
 
 function animate() {
 	requestAnimationFrame( animate );
-	renderer.render( scene, camera );
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
+  renderer.render( scene, camera );
 }
 
 animate();
