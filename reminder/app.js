ffcf9456951ad93dfeeaaaa1e8307ffe072ac38b
Marco Rossel <andresmarcorossel@gmail.com>
2023-05-16 23:24:01 -0400

    three-vite | 5 | first model | reminder | 5

--- reminder/app.js
+++ reminder/app.js
@@ -5,12 +5,17 @@
     const buttonSend = document.querySelector('.btnSend');
     const list = document.querySelector('.list');
 
+    let array = []
+
     buttonSend.addEventListener('click', function (event) {
         event.preventDefault();
+        array.push(inputVal.value);
+        inputVal.value = '';
 
-        list.innerHTML = `<li>${inputVal.value}</li>`
-        console.log(inputVal.value);
+        let dom = '';
+        for (let i = 0; i < array.length; i++) {
+            dom += `<li>${array[i]}</li>`;
+        }
+        list.innerHTML = dom;
     })
-
-
 })()
