830194b74f7100e8f587290a4cb6d940d709d33f
Marco Rossel <andresmarcorossel@gmail.com>
2023-06-24 22:10:29 -0400

    reminder A | 2

--- javaScript-vanilla/reminder/app.js
+++ javaScript-vanilla/reminder/app.js
@@ -1,5 +1,15 @@
 (function () {
     'use strict';
 
-    console.log('archive loaded...');
+    const div = document.querySelector('.div');
+    const input = document.querySelector('.input1');
+    const btn = document.querySelector('.inputBtn');
+
+    btn.addEventListener('click', function (event) {
+        event.preventDefault();
+        console.log(input.value);
+
+        div.innerHTML += `<p>${input.value}</p>`;
+    })
+
 })()
