86da7131640477f8097f0c2155859b05c23c1359
Marco Rossel <andresmarcorossel@gmail.com>
2024-01-18 00:40:32 -0300

    typeScript | Handbook

--- lesson1/build/js/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var username = 'Marco';
-console.log(username);
-var a = 6;
-var b = 2;
-var c = 10;
-console.log(a * c);
-console.log(c / b);
-// viendo los errores de tipo 
---
+++ typescript.ts
@@ -0,0 +1,13 @@
+interface User {
+  name: string;
+  id: number;
+  //username: string;
+}
+
+const user:User = {
+  name: "marco",
+  id: 12,
+  //username: 'lk;sjdf',
+  // muestra error en vscoded si no declaro en la
+  // interface el tipo de dato que sera
+}
