a20d3f11f79bdc24640c85ae5fa11b35d8a6a8df
Marco Rossel <andresmarcorossel@gmail.com>
2024-04-02 16:48:41 -0300

    typeScript tutorial 2024 | 15 challenge

--- 01-typeScript-tutorial-2024/src/main.ts
+++ 01-typeScript-tutorial-2024/src/main.ts
@@ -2,7 +2,7 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
-import './challenges/challenge_14_ckeck_property.ts'
+import './challenges/challenge_15_truthy_falsy.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
